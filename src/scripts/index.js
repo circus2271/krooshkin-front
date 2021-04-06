e7b841888751de017dd710a9bc463fa0932b7386
rodionov80 <rodionov134@gmail.com>
2021-04-07 00:49:07 +0300

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import '../styles/index.scss';
+
+import './includes/muuri-grid';
+import './includes/routing';
+import './includes/header';
+import './includes/sidebar-menu';
+import './includes/photoswipe-gallery';
